c3e8f862516151bba2e35efb2e133908cf3a8283
patmood <patrick.n.moody@gmail.com>
2015-03-27 23:06:31 -0700

    modular refactor

--- script.js
+++ script.js
@@ -1,51 +1,64 @@
-var NUM_MINES = 2
-  , SIZE = 4
+var MineSweep = function(){
+  this.NUM_MINES = 2
+  this.SIZE = 4
+  this.board = [[0, 0, 0, 0]
+               ,[0, 0, 0, 0]
+               ,[0, 0, 0, 0]
+               ,[0, 0, 0, 0]]
 
-function drawBoard() {
-  var board = [[0, 0, 0, 0]
-              ,[0, 0, 0, 0]
-              ,[0, 0, 0, 0]
-              ,[0, 0, 0, 0]]
+  this.drawBoard()
+}
 
-  for (var i=0; i < 2; i++) {
-    var x, y
+MineSweep.prototype = {
+  constructor: MineSweep,
 
-    x = Math.floor(Math.random() * SIZE)
-    y = Math.floor(Math.random() * SIZE)
-    while (board[y][x] === 'X') {
-      x = Math.floor(Math.random() * SIZE)
-      y = Math.floor(Math.random() * SIZE)
+  getPos: function() {
+    return Math.floor(Math.random() * this.SIZE)
+  },
+
+  drawBoard: function() {
+    for (var i=0; i < 2; i++) {
+      var x, y
+
+      // Get initial position
+      x = this.getPos()
+      y = this.getPos()
+
+      // Ensure positions are unique
+      while (this.board[y][x] === 'X') {
+        x = this.getPos()
+        y = this.getPos()
+      }
+      console.log(x, y)
+      this.board[y][x] = "X"
     }
-    console.log(x, y)
-    board[y][x] = "X"
+
+    // TODO: populate the numbers of adjacent mines
+    this.draw()
+
+  },
+
+  draw: function() {
+    var $boardEl = $('#board')
+      , _this = this
+    this.board.forEach(function(row){
+      var rowString = ''
+      row.forEach(function(el) {
+        rowString += '<div class="cell"><div class="cover"><p>' + el + '</p></div></div>'
+      })
+      $boardEl.append('<div class="row">' + rowString + '</div>')
+      $('.cover').on('click', _this.clickHandle)
+    })
+  },
+
+  clickHandle: function(e) {
+    $(this).css('background', 'none').find('p').show()
   }
 
-  console.log(board)
-
-  draw(board)
-  // populate the numbers of adjacent mines
-  return board
 }
 
-function draw(board) {
-  var $board = $('#board')
-  board.forEach(function(row){
-    var rowString = ''
-    row.forEach(function(el) {
-      rowString += '<div class="cell"><div class="cover"><p>' + el + '</p></div></div>'
-    })
-    $board.append('<div class="row">' + rowString + '</div>')
-    $('.cover').on('click', clickHandle)
-  })
-}
-
-function clickHandle(e) {
-  $(e.target).css('background', 'none').find('p').show()
-}
 
 
 $(function(){
-
-  drawBoard()
-
+  new MineSweep()
 })
